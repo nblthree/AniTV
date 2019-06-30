f733ca4eda53849f83ad9a9a7a735814e5d1b1e6
EdelWolle <i-bn@live.fr>
2019-06-30 20:52:21 +0100

    init: check for new Ep each 15min

--- main/index.js
+++ main/index.js
@@ -1,60 +1,60 @@
 // Native
-const { join } = require('path')
-const { format } = require('url')
+const { join } = require('path');
+const { format } = require('url');
 
 // Packages
-const { BrowserWindow, app, ipcMain } = require('electron')
-const isDev = require('electron-is-dev')
-const prepareNext = require('electron-next')
+const { BrowserWindow, app, ipcMain } = require('electron');
+const isDev = require('electron-is-dev');
+const prepareNext = require('electron-next');
 
-const fs = require('fs-extra')
-const fuzz = require('fuzzball')
-const puppeteer = require('puppeteer')
-const WebTorrent = require('webtorrent')
-const Store = require('electron-store')
+const fs = require('fs-extra');
+const fuzz = require('fuzzball');
+const puppeteer = require('puppeteer');
+const WebTorrent = require('webtorrent');
+const Store = require('electron-store');
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 if (!isProd) {
-  const userDataPath = app.getPath('userData')
-  app.setPath('userData', `${userDataPath} (development)`)
+  const userDataPath = app.getPath('userData');
+  app.setPath('userData', `${userDataPath} (development)`);
 }
 
-const client = new WebTorrent()
-const store = new Store({ name: 'appData' })
+const client = new WebTorrent();
+const store = new Store({ name: 'appData' });
 
 // Events Listening..........
 
 // get-set the animes of the current season
 ipcMain.on('get-season', (event, arg) => {
-  event.returnValue = store.get('season') || []
-})
+  event.returnValue = store.get('season') || [];
+});
 ipcMain.on('set-season', (event, arg) => {
-  store.set('season', arg)
-})
+  store.set('season', arg);
+});
 // get the followed animes with additional data like episodes torrent Magnet URI
 ipcMain.on('get-aniList', (event, arg) => {
-  event.returnValue = store.get('aniList') || []
-})
+  event.returnValue = store.get('aniList') || [];
+});
 // Trigger download (triggered by a click on a button)
 ipcMain.on('start-download', (event, { anime, episode }) => {
-  startDownloading(episode.magnet, event, anime)
-})
+  startDownloading(episode.magnet, event, anime);
+});
 // get the followed animes
 ipcMain.on('get-followedAni', (event, arg) => {
-  event.returnValue = store.get('followedAni') || []
-})
-// set the followed animes and update aniList 
+  event.returnValue = store.get('followedAni') || [];
+});
+// set the followed animes and update aniList
 ipcMain.on('set-followedAni', async (event, arg) => {
-  const followedAni = store.get('followedAni') || []
-  followedAni.push(arg)
-  store.set('followedAni', followedAni)
+  const followedAni = store.get('followedAni') || [];
+  followedAni.push(arg);
+  store.set('followedAni', followedAni);
 
   // Episodes
-  const aniList = store.get('aniList') || []
+  const aniList = store.get('aniList') || [];
 
-  const { newTitle, newHashes } = await getAnimeEpisodes(arg)
-  arg.title = newTitle
-  const hashes = newHashes
+  const { newTitle, newHashes } = await getAnimeEpisodes(arg);
+  arg.title = newTitle;
+  const hashes = newHashes;
 
   if (!aniList.some(val => val.mal_id === arg.mal_id)) {
     aniList.push({
@@ -62,226 +62,230 @@ ipcMain.on('set-followedAni', async (event, arg) => {
       title: arg.title,
       episodesNumber: arg.episodes,
       episodes: hashes,
-      watchedEpisodes: 0
-    })
-    store.set('aniList', aniList)
+      watchedEpisodes: 0,
+      lastUpdate: Date.now()
+    });
+    store.set('aniList', aniList);
   }
-})
+});
+// Check for new episodes each 15 minutes
+const inetrval = setInterval(async () => {
+  let aniList = store.get('aniList') || [];
+  if (!aniList.length) return;
+  aniList = await new Promise(resolve => {
+    aniList = aniList.map(async (ele, i) => {
+      const val = ele;
+      const { newTitle, newHashes } = await getAnimeEpisodes(val, val.episodes.length + 1);
+      if (newHashes.length) {
+        val.episodes.push(...newHashes);
+      }
+      return val;
+    });
+    if (i + 1 === aniList.length) {
+      resolve(aniList);
+    }
+  });
+  store.set('aniList', aniList);
+}, 1000 * 60 * 15); // 5min
+
 // Use getHorribleSubs function to search for the episodes
 ipcMain.on('reload-episodes', async (event, arg) => {
-  let aniList = store.get('aniList') || []
-  aniList = aniList.filter(val => val.mal_id !== arg.mal_id)
-  const results = await getHorribleSubs(arg.title)
+  let aniList = store.get('aniList') || [];
+  aniList = aniList.filter(val => val.mal_id !== arg.mal_id);
+  const results = await getHorribleSubs(arg.title);
   aniList.push({
     mal_id: arg.mal_id,
     title: arg.title,
     episodesNumber: arg.episodes,
     episodes: results,
-    watchedEpisodes: 0
-  })
-  store.set('aniList', aniList)
-})
+    watchedEpisodes: 0,
+    lastUpdate: Date.now()
+  });
+  store.set('aniList', aniList);
+});
 // Unfollow an anime.
 ipcMain.on('unset-followedAni', (event, arg) => {
-  let followedAni = store.get('followedAni') || []
-  followedAni = followedAni.filter(val => val.mal_id !== arg.mal_id)
-  store.set('followedAni', followedAni)
-})
+  let followedAni = store.get('followedAni') || [];
+  followedAni = followedAni.filter(val => val.mal_id !== arg.mal_id);
+  store.set('followedAni', followedAni);
+});
 // Get the downloaded episodes
 ipcMain.on('get-downloadedEpi', async (event, arg) => {
-  const aniList = store.get('aniList') || []
-  const torrent = {}
+  const aniList = store.get('aniList') || [];
+  const torrent = {};
   for (const val of aniList) {
     for (const _val of val.episodes) {
       if (_val.pathname && (await fs.pathExists(_val.pathname))) {
         torrent[_val.magnet] = {
           key: _val.magnet,
           progress: 1
-        }
+        };
       }
     }
   }
 
-  event.returnValue = torrent
-})
+  event.returnValue = torrent;
+});
 
 // Functions..............
 function isDuplicate(array, arg) {
-  return array.some(val => val.magnet === arg.magnet)
+  return array.some(val => val.magnet === arg.magnet);
 }
 // Perform different change on the input anime title to make sure that we doesn't get an empty array in most cases
 // Also return an array of the anime epidodes
 // Call chooseHash function and getHashes function
-function getAnimeEpisodes(anime) {
+function getAnimeEpisodes(anime, ep = -1) {
   return new Promise(async resolve => {
-    const titleOperations = [
-      { name: 'normal' },
-      { name: 'drop-nd-rd-th' },
-      { name: 'pure' }
-    ]
+    const titleOperations = [{ name: 'normal' }, { name: 'drop-nd-rd-th' }, { name: 'pure' }];
 
-    const newEpisodesNumber = anime.episodes
-    const newHashes = []
-    const loopLength = Array.apply(null, {
+    const newEpisodesNumber = anime.episodes;
+    const newHashes = [];
+    let loopLength = Array.apply(null, {
       length: 500
-    }).map(Number.call, Number)
-    let newTitle = anime.title
+    }).map(Number.call, Number);
+    let newTitle = anime.title;
     for (const operation of titleOperations) {
       if (operation.name === 'pure') {
-        newTitle = anime.title
-        newTitle = newTitle
-          .replace(/season|nd|part|rd|th|s?\d+/gi, '')
-          .replace(/  +/g, ' ')
+        newTitle = anime.title;
+        newTitle = newTitle.replace(/season|nd|part|rd|th|s?\d+/gi, '').replace(/  +/g, ' ');
       } else if (operation.name === 'normal') {
-        newTitle = anime.title
+        newTitle = anime.title;
         newTitle = newTitle
           .replace(/season ?/i, 'S')
           .replace(/part ?\d/i, '')
           .replace(/\s+0+(?!\.|$)/g, ' ')
-          .replace(/  +/g, ' ')
+          .replace(/  +/g, ' ');
       } else if (operation.name === 'drop-nd-rd-th') {
-        newTitle = anime.title
-        const season = newTitle.match(/\d+/g)
-        newTitle = newTitle.replace(/season|nd|part|rd|th|s?\d+/gi, '')
-        newTitle += season ? ` S${  season[0]}` : ''
-        newTitle = newTitle.replace(/  +/g, ' ')
+        newTitle = anime.title;
+        const season = newTitle.match(/\d+/g);
+        newTitle = newTitle.replace(/season|nd|part|rd|th|s?\d+/gi, '');
+        newTitle += season ? ` S${season[0]}` : '';
+        newTitle = newTitle.replace(/  +/g, ' ');
       }
 
-      newTitle = newTitle.trim()
-
+      newTitle = newTitle.trim();
+      if (ep !== -1) {
+        loopLength = [ep - 1];
+      }
       for (const i of loopLength) {
         const item = chooseHash(await getHashes(newTitle, i + 1), {
           title: newTitle,
           episode: i + 1
-        })
+        });
         if (item && !isDuplicate(newHashes, item)) {
-          newHashes.push({ ...item, number: i + 1 })
+          newHashes.push({ ...item, number: i + 1 });
         } else {
-          break
+          break;
         }
       }
 
-      if (newHashes.length !== 0) break
+      if (newHashes.length !== 0) break;
     }
 
-    resolve({ newTitle, newHashes })
-  })
+    resolve({ newTitle, newHashes });
+  });
 }
 // Return all episodes translated by HorribleSubs from all seasons (Match the anime title without any additions)
 function getHorribleSubs(title) {
   return new Promise(async resolve => {
-    const newTitle =
-      `[HorribleSubs] ${ 
-      title
-        .replace(/season|nd|part|rd|th|s?\d+/gi, '')
-        .replace(/  +/g, ' ')
-        .trim()}`
-    let allMagnets = []
+    const newTitle = `[HorribleSubs] ${title
+      .replace(/season|nd|part|rd|th|s?\d+/gi, '')
+      .replace(/  +/g, ' ')
+      .trim()}`;
+    let allMagnets = [];
     for (let i = 1; i < 50; i++) {
-      const result = await getHashes(newTitle, -1, i)
+      const result = await getHashes(newTitle, -1, i);
       if (result && result.length) {
-        allMagnets.push(...result)
+        allMagnets.push(...result);
       } else {
-        break
+        break;
       }
     }
 
-    allMagnets = allMagnets.map((val, index) => ({ ...val, number: index + 1 }))
-    resolve(allMagnets.sort((a, b) => a.title.localeCompare(b.title)))
-  })
+    allMagnets = allMagnets.map((val, index) => ({ ...val, number: index + 1 }));
+    resolve(allMagnets.sort((a, b) => a.title.localeCompare(b.title)));
+  });
 }
-// Scrap data from nyaa. data contain magnet URI and episode title 
+// Scrap data from nyaa. data contain magnet URI and episode title
 async function getHashes(title, episode, p = 1) {
   const hashes = await new Promise(resolve => {
     void (async () => {
       try {
-        const browser = await puppeteer.launch()
-        const page = await browser.newPage()
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
         await page.goto(
-          `https://nyaa.si/?f=0&c=1_2&q=${processTitle(
-            title,
-            episode
-          )}&p=${p}&s=seeders&o=desc`
-        )
+          `https://nyaa.si/?f=0&c=1_2&q=${processTitle(title, episode)}&p=${p}&s=seeders&o=desc`
+        );
 
         const result = await page.evaluate(() => {
           const grabFromItem = (item, selector, attr) => {
-            const val = item.querySelector(selector)
-            return val ? val[attr] : ''
-          }
+            const val = item.querySelector(selector);
+            return val ? val[attr] : '';
+          };
 
-          const data = document.querySelectorAll('tr')
+          const data = document.querySelectorAll('tr');
           const items = [...data].map(item => ({
-            title: grabFromItem(
-              item,
-              'td:nth-of-type(2) a:not([class])',
-              'title'
-            ),
-            magnet: grabFromItem(
-              item,
-              'td:nth-of-type(3) a:nth-of-type(2)',
-              'href'
-            )
-          }))
-          return items
-        })
-        await browser.close()
-        resolve(result)
+            title: grabFromItem(item, 'td:nth-of-type(2) a:not([class])', 'title'),
+            magnet: grabFromItem(item, 'td:nth-of-type(3) a:nth-of-type(2)', 'href')
+          }));
+          return items;
+        });
+        await browser.close();
+        resolve(result);
       } catch (error) {
-        console.log(error)
-        resolve(getHashes(title, episode))
+        console.log(error);
+        resolve(getHashes(title, episode));
       }
-    })()
-  })
-  return hashes.filter(val => val.title && val.magnet)
+    })();
+  });
+  return hashes.filter(val => val.title && val.magnet);
 }
 
-// Use fuzz search to get the right anime episode 
+// Use fuzz search to get the right anime episode
 function chooseHash(hashes, { title, episode }) {
-  title += ` ${  episode}`
+  title += ` ${episode}`;
   const options = {
     scorer: fuzz.token_set_ratio,
     returnObjects: true,
     full_process: true
-  }
+  };
 
-  const choices = hashes.map(val => val.title.replace(/\s+0+(?!\.|$)/g, ' '))
-  const result = fuzz.extract(title, choices, options)
+  const choices = hashes.map(val => val.title.replace(/\s+0+(?!\.|$)/g, ' '));
+  const result = fuzz.extract(title, choices, options);
 
   const key =
     result.length > 1
       ? result[0].key > result[1].key && result[0].score === result[1].score
         ? result[1].key
         : result[0].key
-      : 0
-  return hashes[key]
+      : 0;
+  return hashes[key];
 }
 
 // Add some info to the title and return the encoded title value
 function processTitle(title, episode) {
-  const quality = 720
+  const quality = 720;
   if (episode !== -1) {
-    title += ` ${  episode  } ${  quality}`
+    title += ` ${episode} ${quality}`;
   } else {
-    title += ` ${  quality}`
+    title += ` ${quality}`;
   }
 
-  title = fixedEncodeURI(title)
-  return title
+  title = fixedEncodeURI(title);
+  return title;
 }
 // Encode the title to be used by puppeteer
 function fixedEncodeURI(str) {
   return encodeURI(str)
     .replace(/[!'()*]/g, escape)
-    .replace(/%20/g, '+')
+    .replace(/%20/g, '+');
 }
 // Download anime episodes
 function startDownloading(magnet, event, anime) {
-  const folder = anime.title
-  const magnetURI = magnet
-  const downloadPath = app.getPath('downloads')
-  const pathname =
-    downloadPath + (downloadPath.endsWith('/') ? '' : '/') + folder
+  const folder = anime.title;
+  const magnetURI = magnet;
+  const downloadPath = app.getPath('downloads');
+  const pathname = downloadPath + (downloadPath.endsWith('/') ? '' : '/') + folder;
   client.add(magnetURI, { path: pathname }, function(torrent) {
     event.sender.send('torrent-progress', {
       key: magnet,
@@ -289,34 +293,34 @@ function startDownloading(magnet, event, anime) {
       downloaded: 0,
       speed: 0,
       progress: 0
-    })
-    console.log('Client is downloading:', torrent.infoHash)
+    });
+    console.log('Client is downloading:', torrent.infoHash);
 
     torrent.on('done', function() {
-      console.log('torrent download finished')
-      let aniList = store.get('aniList') || []
-      let { episodes } = aniList.filter(val => val.mal_id === anime.mal_id)[0]
+      console.log('torrent download finished');
+      let aniList = store.get('aniList') || [];
+      let { episodes } = aniList.filter(val => val.mal_id === anime.mal_id)[0];
       episodes = episodes.map(val => {
         if (val.magnet === magnetURI) {
           for (let i = 0; i < torrent.files.length; i++) {
-            val.pathnames[i] = `${pathname  }/${  torrent.files[i].path}`
+            val.pathnames[i] = `${pathname}/${torrent.files[i].path}`;
           }
         }
 
-        return val
-      })
+        return val;
+      });
       aniList = aniList.map(val => {
         if (val.mal_id === anime.mal_id) {
-          val.episodes = episodes
+          val.episodes = episodes;
         }
 
-        return val
-      })
-      store.set('aniList', aniList)
-    })
+        return val;
+      });
+      store.set('aniList', aniList);
+    });
     torrent.on('error', function(err) {
-      console.log(err)
-    })
+      console.log(err);
+    });
     torrent.on('download', function(bytes) {
       event.sender.send('torrent-progress', {
         key: magnet,
@@ -324,14 +328,14 @@ function startDownloading(magnet, event, anime) {
         downloaded: torrent.downloaded,
         speed: torrent.downloadSpeed,
         progress: torrent.progress
-      })
-    })
-  })
+      });
+    });
+  });
 }
 
 // Prepare the renderer once the app is ready
 app.on('ready', async () => {
-  await prepareNext('./renderer')
+  await prepareNext('./renderer');
 
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -342,8 +346,8 @@ app.on('ready', async () => {
       contextIsolation: false,
       preload: join(__dirname, 'preload.js')
     }
-  })
-  mainWindow.webContents.openDevTools()
+  });
+  mainWindow.webContents.openDevTools();
 
   const url = isDev
     ? 'http://localhost:8000/start'
@@ -351,15 +355,15 @@ app.on('ready', async () => {
         pathname: join(__dirname, '../renderer/start/index.html'),
         protocol: 'file:',
         slashes: true
-      })
-  mainWindow.setMenu(null)
-  mainWindow.loadURL(url)
-})
+      });
+  mainWindow.setMenu(null);
+  mainWindow.loadURL(url);
+});
 
 // Quit the app once all windows are closed
-app.on('window-all-closed', app.quit)
+app.on('window-all-closed', app.quit);
 
 // listen the channel `message` and resend the received message to the renderer process
 ipcMain.on('message', (event, message) => {
-  event.sender.send('message', message)
-})
+  event.sender.send('message', message);
+});
